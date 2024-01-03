7af6801ea425c462f1542ca66b76a17f99daf314
dan1oo <wangda2022@gmail.com>
2024-01-02 21:51:45 -0600

    First commit XD

---
+++ mysite/poker/gamelogic/card.py
@@ -0,0 +1,51 @@
+import random
+
+#class for individual card
+values = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10, "J":11, "Q":12, "K":13, "A":14}
+suits = {"Club", "Heart", "Spade", "Diamond"}
+class Cards():
+
+    def __init__(self,suit: None, rank: None):
+        self.suit = suit
+        self.rank = rank
+
+    def returnsuit(self):
+        return self.suit
+    
+    def returnrank(self):
+
+        return self.rank
+# class for deck with randomizer
+class Deck():
+
+    def __init__(self):
+
+        self._deck = [Cards(suit ,rank) for rank in values for suit in suits]
+
+
+    def shuffle(self):
+        random.shuffle(self._deck)
+
+    #testing random
+    def __str__(self):
+        deckstring = [f'{cards.rank}{cards.suit}' for cards in self._deck]
+        prinstring = deckstring[:6]
+        return str(prinstring)
+    
+
+deck = Deck()
+for x in range(10):
+    print(deck)
+    deck.shuffle()
+
+
+
+
+
+
+
+    
+
+
+    
+
